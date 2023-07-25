66b5c15a40c8d366c6c7a9b9b81d4a77b8774526
pongpet <pongpet.m@gmail.com>
2023-07-25 22:29:35 +0700

    Update install-ckanext-exat.sh

--- scripts/install-ckanext-exat.sh
+++ scripts/install-ckanext-exat.sh
@@ -19,6 +19,7 @@ ckan config-tool ${CKAN_INI} "ckanext.exat.security_center.client = ckanext.exat
 ckan config-tool ${CKAN_INI} "ckanext.exat.assign_default_organization = true"
 ckan config-tool ${CKAN_INI} "ckanext.exat.assign_personnel_organization = false"
 ckan config-tool ${CKAN_INI} "ckanext.exat.override_stats = true"
+ckan config-tool ${CKAN_INI} "ckan.plugins = discovery search_suggestions exat thai_gdc stats image_view text_view recline_view resource_proxy webpage_view datastore xloader dga_stats scheming_datasets pdf_view hierarchy_display hierarchy_form dcat dcat_json_interface structured_data pages"
 
 deactivate
 
